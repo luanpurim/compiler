36e324fd748f1a1bd9f9684aefb9c5cafaefba86
Luan <luancarlospurim@gmail.com>
2018-09-24 19:50:47 -0300

    adicionando fontes do gals

---
+++ src/gals/Constants.java
@@ -0,0 +1,49 @@
+package gals;
+
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_identificador = 2;
+    int t_reservada = 3;
+    int t_and = 4;
+    int t_array = 5;
+    int t_do = 6;
+    int t_exit = 7;
+    int t_false = 8;
+    int t_if = 9;
+    int t_ifFalseDo = 10;
+    int t_ifTrueDo = 11;
+    int t_literal = 12;
+    int t_logical = 13;
+    int t_number = 14;
+    int t_not = 15;
+    int t_or = 16;
+    int t_read = 17;
+    int t_true = 18;
+    int t_write = 19;
+    int t_constante_numérica = 20;
+    int t_constante_caractere = 21;
+    int t_constante_literal = 22;
+    int t_TOKEN_23 = 23; //":"
+    int t_TOKEN_24 = 24; //","
+    int t_TOKEN_25 = 25; //"["
+    int t_TOKEN_26 = 26; //"]"
+    int t_TOKEN_27 = 27; //"{"
+    int t_TOKEN_28 = 28; //"}"
+    int t_TOKEN_29 = 29; //"("
+    int t_TOKEN_30 = 30; //")"
+    int t_TOKEN_31 = 31; //":="
+    int t_TOKEN_32 = 32; //"+"
+    int t_TOKEN_33 = 33; //"-"
+    int t_TOKEN_34 = 34; //"*"
+    int t_TOKEN_35 = 35; //"/"
+    int t_TOKEN_36 = 36; //"="
+    int t_TOKEN_37 = 37; //"!="
+    int t_TOKEN_38 = 38; //"<"
+    int t_TOKEN_39 = 39; //"<="
+    int t_TOKEN_40 = 40; //">"
+    int t_TOKEN_41 = 41; //">="
+
+}
