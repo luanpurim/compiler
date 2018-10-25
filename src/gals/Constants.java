b04080cdbcc3ab3eef3ccee261302e0a7cf5e832
Adriner Andrade <adriner_andrade@hotmail.com>
2018-10-25 19:30:45 -0300

    parte sintática

--- src/gals/Constants.java
+++ src/gals/Constants.java
@@ -1,6 +1,6 @@
 package gals;
 
-public interface Constants extends ScannerConstants
+public interface Constants extends ScannerConstants, ParserConstants
 {
     int EPSILON  = 0;
     int DOLLAR   = 1;
---
+++ src/gals/Semantico.java
@@ -0,0 +1,9 @@
+package gals;
+
+public class Semantico implements Constants
+{
+    public void executeAction(int action, Token token)	throws SemanticError
+    {
+        System.out.println("Ação #"+action+", Token: "+token);
+    }	
+}
