cd3bff9d5921172d6c4e96ce9daee5246aa04d6a
Adriner Andrade <adriner_andrade@hotmail.com>
2018-11-29 20:12:23 -0200

    erro semantico

--- src/gals/SemanticError.java
+++ src/gals/SemanticError.java
@@ -2,13 +2,10 @@ package gals;
 
 public class SemanticError extends AnalysisError
 {
-    public SemanticError(String msg, int position)
-	 {
-        super(msg, position);
-    }
 
     public SemanticError(String msg)
     {
         super(msg);
     }
+
 }
