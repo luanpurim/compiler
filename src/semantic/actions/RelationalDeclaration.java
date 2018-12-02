960a10cc390a8c4457eb730d13e60c6385b333bd
Luan.Purim <luan.purim@senior.com.br>
2018-12-02 15:02:25 -0200

    operador relacional

---
+++ src/semantic/actions/RelationalDeclaration.java
@@ -0,0 +1,20 @@
+package semantic.actions;
+
+import gals.SemanticError;
+import gals.Token;
+import semantic.Context;
+import semantic.Variable;
+
+public class RelationalDeclaration implements SemanticActionHandler{
+
+	@Override
+	public int id() {
+		return 9;
+	}
+
+	@Override
+	public void handle(Token token, Context context) throws SemanticError {
+		context.getVariables().set(Variable.RELATIONAL_OPERATOR, token.getLexeme());
+	}
+
+}
---
+++ src/semantic/actions/RelationalExecution.java
@@ -0,0 +1,40 @@
+package semantic.actions;
+
+import gals.SemanticError;
+import gals.Token;
+import semantic.Context;
+import semantic.MsilCommand;
+import semantic.VarType;
+import semantic.Variable;
+
+public class RelationalExecution implements SemanticActionHandler{
+
+	@Override
+	public int id() {
+		return 10;
+	}
+
+	@Override
+	public void handle(Token token, Context context) throws SemanticError {
+		VarType t1 = context.getTypeQueue().poll();
+		VarType t2 = context.getTypeQueue().poll();
+		if(t1 != t2) {
+			throw new SemanticError("Mensagem de erro de relacional");
+		} 
+		context.getTypeQueue().add(VarType.LOGICAL);
+		switch ((String) context.getVariables().get(Variable.RELATIONAL_OPERATOR)) {
+		case ">":
+			context.getCode().add(MsilCommand.GREATER_THAN);
+			break;
+		case "<":
+			context.getCode().add(MsilCommand.LESS_THAN);
+			break;
+		case "=":
+			context.getCode().add(MsilCommand.EQUALS);
+			break;
+		default:
+			throw new SemanticError("Operador não encontrado");
+		}
+	}
+
+}
