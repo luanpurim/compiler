ea3954c5fcea9e3122f11f339d02dd6f96e0df17
Luan <luancarlospurim@gmail.com>
2018-12-03 20:44:45 -0200

    actions 19,20

---
+++ src/semantic/actions/Action019.java
@@ -0,0 +1,22 @@
+package semantic.actions;
+
+import gals.SemanticError;
+import gals.Token;
+import semantic.Context;
+import semantic.MsilCommand;
+import semantic.VarType;
+
+public class Action019 implements SemanticActionHandler{
+
+	@Override
+	public int id() {
+		return 19;
+	}
+
+	@Override
+	public void handle(Token token, Context context) throws SemanticError {
+		context.getTypeQueue().add(VarType.LITERAL);
+        context.getCode().add(MsilCommand.PUSH_LITERAL, token.getLexeme());
+	}
+
+}
