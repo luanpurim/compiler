9e579cc1918449899f97c13cdd17bb874e977eee
Luan <luancarlospurim@gmail.com>
2018-12-03 19:26:41 -0200

    ajuste action 103

--- src/semantic/actions/Action103.java
+++ src/semantic/actions/Action103.java
@@ -14,14 +14,16 @@ public class Action103 implements SemanticActionHandler{
 	}
 
 	@Override
-	public void handle(Token token, Context context) throws SemanticError {
-		context.getIdentifiersList().forEach(id -> {
+	public void handle(Token token, Context context) throws SemanticError {		
+		for(int i = 0; i <= context.getIdentifiersList().size(); i++ ) {
+			String id = context.getIdentifiersList().get(i);
 			if(context.getSymbolTable().containsKey(id)) {
 				throw new SemanticError("Simbolo já existe na tabela de simbolos");
 			}
 			context.getSymbolTable().put(token.getLexeme(), context.getVariables().get(Variable.TYPE_DECLARATION));
 			context.getCode().add(MsilCommand.LOCALS, context.getVariables().get(Variable.TYPE_DECLARATION), token.getLexeme());
-		});
+			
+		}
 		context.getIdentifiersList().clear();
 	}
 	
